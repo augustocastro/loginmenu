b49d56861c6fc15ddb032997c90b7a780c44430c
augustocastro <augusto1castro@gmail.com>
2019-08-21 23:04:22 -0300

    ajustes.

--- src/app/model/Login.ts
+++ src/app/model/Login.ts
@@ -1,7 +1,5 @@
 export class Login {
 
-    constructor(public email: string, public senha: string) {
-        this.email = email;
-        this.senha = senha;
-    }
+    email: string;
+    senha: string;
 }
